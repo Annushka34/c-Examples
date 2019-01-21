4fde299b012170608e6b42bc4928fa8820e759a2
annushka34 <annsamoluk82@gmail.com>
2019-01-21 11:57:13 +0200

    Functors, Predicats

--- 32стаціонар/19_map/19_map/06_Functors.cpp
+++ 32стаціонар/19_map/19_map/06_Functors.cpp
@@ -1,141 +1,141 @@
-#include<iostream>
-#include<Windows.h>
-#include<map>
-#include<unordered_map>
-#include<string>
-#include<iomanip>
-#include<set>
-#include<algorithm>
-#include<array>
-
-using namespace std;
-void SetCol(int col)
-{
-	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), col);
-}
-void Print(string w)
-{
-	SetCol(10);
-	cout << "\n---------------------" << w << "------------------------\n";
-	SetCol(7);
-}
-
-bool Compare(int x, int y)
-{
-	return (x > y);
-}
-bool MyCompareString(string x, string y)
-{
-	int len1 = x.length();
-	int len2 = x.length();
-	return (x[len1-1] < y[len2-1]);
-}
-
-
-class Person
-{
-public:
-	int age;
-	string name;
-	double salary;
-	Person(int a, string n, double s)
-	{
-		age = a;
-		name = n;
-		salary = s;
-	}
-	Person() = default;
-	friend ostream & operator << (ostream & out, const Person & p);
-};
-ostream & operator << (ostream & out, const Person & p)
-{
-	return out << p.name << " " << p.age<< " "<<p.salary << endl;
-}
-
-
-bool ComparePerson(const Person &p1, const Person &p2)
-{
-	return (p1.age < p2.age);
-}
-
-bool ComparePersonByName(const Person &p1, const Person &p2)
-{
-	return (p1.name < p2.name);
-}
-
-//void Swap(int &x, int &y)
+//#include<iostream>
+//#include<Windows.h>
+//#include<map>
+//#include<unordered_map>
+//#include<string>
+//#include<iomanip>
+//#include<set>
+//#include<algorithm>
+//#include<array>
+//
+//using namespace std;
+//void SetCol(int col)
 //{
-//	int t = x;
-//	x = y;
-//	y = t;
+//	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), col);
+//}
+//void Print(string w)
+//{
+//	SetCol(10);
+//	cout << "\n---------------------" << w << "------------------------\n";
+//	SetCol(7);
 //}
 //
-//void Sort(array<int, 7> &arr, int size)
+//bool Compare(int x, int y)
 //{
-//	for (size_t i = 0; i < size; i++)
+//	return (x > y);
+//}
+//bool MyCompareString(string x, string y)
+//{
+//	int len1 = x.length();
+//	int len2 = x.length();
+//	return (x[len1-1] < y[len2-1]);
+//}
+//
+//
+//class Person
+//{
+//public:
+//	int age;
+//	string name;
+//	double salary;
+//	Person(int a, string n, double s)
 //	{
-//		for (size_t j = 0; j < size-i - 1; j++)
+//		age = a;
+//		name = n;
+//		salary = s;
+//	}
+//	Person() = default;
+//	friend ostream & operator << (ostream & out, const Person & p);
+//};
+//ostream & operator << (ostream & out, const Person & p)
+//{
+//	return out << p.name << " " << p.age<< " "<<p.salary << endl;
+//}
+//
+//
+//bool ComparePerson(const Person &p1, const Person &p2)
+//{
+//	return (p1.age < p2.age);
+//}
+//
+//bool ComparePersonByName(const Person &p1, const Person &p2)
+//{
+//	return (p1.name < p2.name);
+//}
+//
+////void Swap(int &x, int &y)
+////{
+////	int t = x;
+////	x = y;
+////	y = t;
+////}
+////
+////void Sort(array<int, 7> &arr, int size)
+////{
+////	for (size_t i = 0; i < size; i++)
+////	{
+////		for (size_t j = 0; j < size-i - 1; j++)
+////		{
+////			if (Compare(arr[j], arr[j+1]))
+////			{
+////				Swap(arr[j], arr[j + 1]);
+////			}
+////		}
+////	}
+////}
+//
+//
+//void main()
+//{
+//	array<int, 7> arr = { 2,22,5,7,8,13, 5 };
+//	//Sort(arr, 7);
+//
+//	sort(arr.begin(), arr.end(), Compare);
+//
+//
+//	sort(arr.begin(), arr.end(), [](int x, int y) { return x > y; });
+//	for (auto el: arr)
+//	{
+//		cout << el << " ";
+//	}
+//
+//	Print("STRING COMPARE");
+//	vector<string> v{ "motherb","fathera","sister","brother","babby" };
+//	sort(v.begin(), v.end(), MyCompareString);
+//	for (auto el : v)
+//	{
+//		cout << el << "\n";
+//	}
+//
+//	Print("PERSON COMPARE");
+//	vector<Person> lP
+//	{
+//		{10, "Petja", 2000.52},
+//		{22, "Olga",12000},
+//		{33, "Ivan",8000},
+//	};
+//	lP.push_back(Person(25, "Petro",6500));
+//	for (auto el : lP)
+//	{
+//		cout << el;
+//	}
+//	//sort(lP.begin(), lP.end(), ComparePerson);
+//	sort(lP.begin(), lP.end(), [](const Person &p1, const Person &p2) {return p1.salary < p2.salary; });
+//	Print("AFTER SORT");
+//	for (auto el : lP)
+//	{
+//		cout << el;
+//	}
+//
+//	string word_start = "hello";
+//	string word = word_start;
+//	while (word.length() != 2)
+//	{
+//		do
 //		{
-//			if (Compare(arr[j], arr[j+1]))
-//			{
-//				Swap(arr[j], arr[j + 1]);
-//			}
-//		}
+//			cout << word << endl;
+//		} while (next_permutation(word.begin(), word.end()));
+//		word_start.pop_back();
+//		word = word_start;
 //	}
 //}
-
-
-void main()
-{
-	array<int, 7> arr = { 2,22,5,7,8,13, 5 };
-	//Sort(arr, 7);
-
-	sort(arr.begin(), arr.end(), Compare);
-
-
-	sort(arr.begin(), arr.end(), [](int x, int y) { return x > y; });
-	for (auto el: arr)
-	{
-		cout << el << " ";
-	}
-
-	Print("STRING COMPARE");
-	vector<string> v{ "motherb","fathera","sister","brother","babby" };
-	sort(v.begin(), v.end(), MyCompareString);
-	for (auto el : v)
-	{
-		cout << el << "\n";
-	}
-
-	Print("PERSON COMPARE");
-	vector<Person> lP
-	{
-		{10, "Petja", 2000.52},
-		{22, "Olga",12000},
-		{33, "Ivan",8000},
-	};
-	lP.push_back(Person(25, "Petro",6500));
-	for (auto el : lP)
-	{
-		cout << el;
-	}
-	//sort(lP.begin(), lP.end(), ComparePerson);
-	sort(lP.begin(), lP.end(), [](const Person &p1, const Person &p2) {return p1.salary < p2.salary; });
-	Print("AFTER SORT");
-	for (auto el : lP)
-	{
-		cout << el;
-	}
-
-	string word_start = "hello";
-	string word = word_start;
-	while (word.length() != 2)
-	{
-		do
-		{
-			cout << word << endl;
-		} while (next_permutation(word.begin(), word.end()));
-		word_start.pop_back();
-		word = word_start;
-	}
-}
---
+++ 32стаціонар/19_map/19_map/07_Functors.cpp
@@ -0,0 +1,143 @@
+#include<iostream>
+#include<Windows.h>
+#include<map>
+#include<unordered_map>
+#include<string>
+#include<iomanip>
+#include<set>
+#include<algorithm>
+#include<array>
+#include<vector>
+#include<list>
+
+using namespace std;
+void SetCol(int col)
+{
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), col);
+}
+void Print(string w)
+{
+	SetCol(10);
+	cout << "\n---------------------" << w << "------------------------\n";
+	SetCol(7);
+}
+
+bool HowCopy(string str)
+{
+	/*if (tolower(str.at(0)) == 'a')
+		return true;
+	else
+		return false;*/
+	return tolower(str.at(0)) == 'a';
+}
+
+//class MyClass
+//{
+//	string name = "Vasja";
+//	int arr[5];
+//public:
+//	void operator ()(string s)
+//	{
+//		name = s;
+//	}
+//	MyClass()
+//	{
+//		for (int i = 0; i< 5; i++)
+//		{
+//			arr[i] = i;
+//		}
+//	}
+//	void operator[](int ind)
+//	{
+//		cout << arr[ind];
+//	}
+//	void Show()
+//	{
+//		cout << name;
+//	}
+//};
+
+class Pred
+{
+public:
+	bool operator()(int num)
+	{
+		//PrintOneEl(num);
+		return num > 10;
+	}
+
+	void PrintOneEl(int el)
+	{
+		cout << "*" << el << "*" << endl;
+	}
+};
+
+class Func
+{
+public:
+	void operator() (int num)
+	{
+		cout << "[";
+		if (num %2 == 0)
+		{
+			cout << num;
+		}
+		cout << "]" << endl;
+	}
+};
+
+void main()
+{
+	//-----------CASE 1: ----------USE PREDICAT AS SIMPLE FUNCTION--------------
+	array<string, 6> arr{ "ahello","bye","Amother","afather","sister","AA123456" };
+	array<string, 6> arr2;
+	array<string, 6> arr3;
+	array<string, 6>::iterator itBeg = arr2.begin();
+	copy(arr.begin(), arr.end(), itBeg);
+	copy(arr2.begin(), arr2.end(), ostream_iterator<string>(cout, ","));
+	Print("COPY IF: ");
+	copy_if(arr.begin(), arr.end(), arr3.begin(), HowCopy);
+	copy(arr3.begin(), arr3.end(), ostream_iterator<string>(cout, ","));
+
+	Print("COUNT BYE: ");
+	cout<< count(arr.begin(), arr.end(), "bye");
+	cout << count_if(arr.begin(), arr.end(), HowCopy);
+
+
+	//-------CASE 2: ------USE PREDICAT AS LAMBDA----------
+	vector<string> v{ "ahello","bye","Amother","afather","sister","AA123456" };
+	copy(v.begin(), v.end(), ostream_iterator<string>(cout, ","));
+	Print("ALL LENGTH > 5");
+	copy_if(v.begin(), v.end(), ostream_iterator<string>(cout, ","), [](string el) {return el.length() > 5; });
+
+
+	//------CASE 3: ----------USE PREDICAT AS CLASS WITH SCOPES OVERLOAD---
+	Print("Scopes overload:");
+	/*MyClass myClassObj;
+	myClassObj[2];
+	myClassObj("Olga");
+	myClassObj.Show();*/
+
+	list<int> li{ 2,5,10,44,6,72,8 };
+	Pred p;
+	copy_if(li.begin(), li.end(), ostream_iterator<int>(cout, ","), p);
+	cout << endl;
+	copy_if(li.begin(), li.end(), ostream_iterator<int>(cout, ","), Pred());
+
+
+	//-----FOR_EACH----
+	Print("-FOR_EACH-:");
+	for_each(li.begin(), li.end(), [](int el) { cout << " -- " << el << " -- "; });
+	cout << endl;
+	for_each(li.begin(), li.end(), [](int el) { cout << " -- " << el*2 << " -- "; });
+	cout << endl;
+	for_each(li.begin(), li.end(), [](int el) { cout << " -- " << (el > 10? to_string(el) : " * ") << " -- "; });
+	cout << endl;
+	//----DON'T CHANGE IF NOT REFERENSED---
+	for_each(li.begin(), li.end(), [](int &el) { el *= 10; });
+	for_each(li.begin(), li.end(), [](int el) { cout << " -- " << el << " -- "; });
+	//-----------------
+	for_each(li.begin(), li.end(), Func());
+	cout << endl;
+	cout<<GetConsoleCP();
+}
